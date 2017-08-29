f9745f1b2a1197a65c9d36baa39bc370e680cba8
Ajax1234 <pentester18@gmail.com>
2017-08-29 15:25:48 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'WikiParser',
+    packages = ['WikiParser'],
+    version = '1.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Python library to scrape Wikipedia Data',
+    author = 'James Petullo',
+    author_email = 'jpetullo14@gmail.com',
+    url = 'https://github.com/Ajax12345/WikiParser',
+    download_url = 'https://github.com/Ajax12345/WikiParser/archive/1.0.tar.gz',
+    keywords = ['Python', 'Wikipedia', 'BeautifulSoup'],
+    classifiers = [],
+)
 
